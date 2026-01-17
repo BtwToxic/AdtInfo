311a76f5cb3cfe23d1286ea9ff04c16e15ff8952
BtwToxic <btwalexxd@gmail.com>
2026-01-17 20:36:31 +0530

    Update and rename bot to bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Callb
 
 # --- CONFIGURATION ---
 TELEGRAM_BOT_TOKEN = "8549965128:AAFfyPIMiNvYPFP3qEHZb_VEw8tL7GIN62I"
-NUM_INFO_API_KEY = "9ea6615bf30c4890"
+NUM_INFO_API_KEY = "33175dfa58d94958"
 API_BASE_URL = "https://daily-binny-ryuioggv-391a9381.koyeb.app/api/lookup"
 
 # --- LOGGING SETUP ---
